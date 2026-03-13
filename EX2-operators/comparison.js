53cb52c917451872139acc296cbda9f7bd642101
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-13 09:05:52 +0300

    EX2 comparison

---
+++ EX2-operators/comparison.js
@@ -0,0 +1,17 @@
+//  exercise comparison 
+
+let f = 15;
+let h = '15';
+
+
+console.log( f == h); 
+console.log(f === h); 
+
+console.log(f != h); 
+console.log(f !== h); 
+
+console.log(f > h);
+console.log(f >= h); 
+
+console.log(f < h); 
+console.log(f <= h); 
