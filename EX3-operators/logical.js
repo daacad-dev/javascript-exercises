9800b96fe3243ae68fda39a1ba8d546c1031350e
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-13 09:42:24 +0300

    EX3 logical operators

---
+++ EX3-operators/logical.js
@@ -0,0 +1,13 @@
+// exercise logical operators
+
+// Question mark: hdee true ku soo baxdo xeebta waad adi kartaa
+
+
+let isSunny= true;
+let isWeekend= false;
+
+console.log(isSunny && isWeekend); //output false.  mala aadi karo
+
+console.log(isSunny || isWeekend); // output true. waa la aadi karaa
+
+console.log(!isSunny); // output false.
