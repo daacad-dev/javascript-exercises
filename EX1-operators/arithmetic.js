a8142f97758a61249c0cb4e57a19cc9ce00fe7b9
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-13 07:51:54 +0300

    EX1 arithmetic

---
+++ EX1-operators/arithmetic.js
@@ -0,0 +1,15 @@
+// operators
+
+// exercise arithmetic opwerator
+
+let x = 7;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+console.log( ++x);
+console.log( --y);
