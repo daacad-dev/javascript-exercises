f5e2aad0335cce1f804af01ea3031e05cc065fff
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-15 22:27:38 +0300

    objects for access

---
+++ EX8-objects/objects.js
@@ -0,0 +1,12 @@
+// exercise 8 objects accessing
+
+let car ={
+    make:'toyota',
+    model:'carolla',
+    year:2026,
+}
+
+console.log(car.make);
+console.log(car.model);
+
+console.log(car['year']);
