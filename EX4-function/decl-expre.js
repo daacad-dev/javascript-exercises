0ea79bb524ef1f7c65a9c4876c7b90a7f9641582
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-15 06:42:50 +0300

    EX4 function

---
+++ EX4-function/decl-expre.js
@@ -0,0 +1,29 @@
+// exercise 4 funcrion
+// Q1: function declarition
+
+function add(a,b) {
+    return a + b;
+}
+
+let result1= add(3,2);
+let result2= add(99,1);
+let result3= add(53,18);
+
+console.log( result1);
+console.log( result2);
+console.log( result3);
+
+
+// Q2 function expression
+
+const mult = function(x,y) {
+    return x * y;
+}
+
+let total1 = mult(2,5);
+let total2 = mult(5,5);
+let total3 = mult(212,303);
+
+console.log(total1);
+console.log(total2);
+console.log(total3);
