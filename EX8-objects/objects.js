bcd9ee4cc438a5ccde8ee471727b454e33387608
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-15 23:23:32 +0300

    object methods

--- EX8-objects/objects.js
+++ EX8-objects/objects.js
@@ -10,3 +10,18 @@ console.log(car.make);
 console.log(car.model);
 
 console.log(car['year']);
+
+// exercise 9 object methods
+
+let car2 ={
+    make: 'ferrari',
+    model:' luxury sports car',
+    year:2025,
+    start: function(){
+        console.log('The car has started');
+    }
+}
+
+console.log(car2);
+
+console.log(car2.start());
