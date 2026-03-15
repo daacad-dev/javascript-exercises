d5760f50a9df1e79c4fdf9c96142b973369b9713
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-15 07:07:04 +0300

    arrow function

---
+++ EX5-function/arrow.js
@@ -0,0 +1,13 @@
+// exercise 5 arrow function
+
+let sub = (x,y) => {
+    return x - y;
+}
+
+let total1= sub(10,1);
+let total2= sub(191,98);
+let total3= sub(100,50);
+
+console.log(total1);
+console.log(total2);
+console.log(total3);
