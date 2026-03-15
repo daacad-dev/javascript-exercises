2bbf73195c30af8df6f8295a025aa4bb25369763
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-16 02:31:51 +0300

    for loop

--- EX11-loops/loop.js
+++ EX11-loops/loop.js
@@ -3,3 +3,11 @@
 for(let m=1; m <= 50; m++) {
     console.log(m);
 }
+
+// exercise 12 for loop in arrays
+
+let fruit = ['apple', 'banana', 'mango'];
+
+for(let i = 0; i < fruit.length; i++) {
+    console.log('fruit is: ' +fruit[i]);
+}
