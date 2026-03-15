bb181edda6ad878a75bb27b45bdf59bdb119ace8
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-15 17:17:47 +0300

    arrays methods

--- EX6-arrays/arrays.js
+++ EX6-arrays/arrays.js
@@ -5,3 +5,19 @@ let colors = ['black', 'blue', 'white'];
 console.log(colors[2]);
 console.log(colors[0]);
 console.log(colors[1]);
+
+console.log(colors);
+console.log('before');
+
+
+// exercise 7 arrays methods
+
+colors.push('red');
+console.log(colors);
+
+console.log('after push');
+
+colors.shift();
+console.log(colors);
+
+console.log('after shift');
