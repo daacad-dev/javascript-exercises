783e3cb21741011a624ad02e59e7b725b80410db
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-22 09:09:10 +0300

    array & object destructuring

--- EX22-Basic-ES6/destructuring.js
+++ EX22-Basic-ES6/destructuring.js
@@ -4,3 +4,11 @@ let colors=['blue', 'green', 'red'];
 
 let [c1, c2, c3] = colors;
     console.log(`Best colors is: ${c1} and ${c3}`);
+
+
+// exercise 23 object destructuring
+let car = { make: "Toyota", model: "Corolla", year: 2020 };
+
+let {make, model, year} = car;
+    console.log(make);
+    console.log(model);
