dd4338cb9be8fb809a645c6ddb42c458eec7adf4
daacad-dev <qanciyecadeh1@gmail.com>
2026-03-23 16:43:29 +0300

    promise with async/await

--- EX26-Asynchronous/promise.js
+++ EX26-Asynchronous/promise.js
@@ -5,7 +5,7 @@ function fetchDataWithPromise() {
     return new Promise((resolve, reject) => {
 
         setTimeout(() => {
-            
+
             const success = true; 
 
             if (success) {
@@ -21,3 +21,40 @@ function fetchDataWithPromise() {
 fetchDataWithPromise()
     .then(message => console.log(message))
     .catch(error => console.error(error));
+
+
+
+// exercise 28 async/await
+
+function fetchDataWithAsync_Await() {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            
+            const success = false; 
+
+            if (success) {
+                resolve("Data fetched successfully!");
+            } else {
+                reject("Failed to fetch data.");
+            };
+
+        }, 3000);
+    });
+}
+
+async function displayData() {
+    
+    try{
+
+        let data = await fetchDataWithAsync_Await();
+            console.log(data);
+
+    }catch(error){
+
+        console.log(error);
+    }
+}
+
+displayData();
